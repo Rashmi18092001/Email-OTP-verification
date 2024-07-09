f0d957e2a7f2eecefaec55e6d483d524a3befd81
Rashmi Jadhav <jrashmi853@gmail.com>
2024-07-09 15:49:02 +0530

    making changes

--- form/vite.config.js
+++ form/vite.config.js
@@ -1,8 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // Ensure this is the correct output directory
+  },
   base: "/form/"
 })
+
